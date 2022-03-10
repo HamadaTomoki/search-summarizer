8b3783dbb95107e6350b3d30e210036f0fdb05c2
hamadatomoki <hamada.tomoki01@gmail.com>
2022-03-11 02:20:33 +0900

    :necktie: Update app link business logic
    appとserverを関連づけるlogicを変更
    Digital Asset Linksのjson fileを追加し、appとserverを紐付けた

--- src/main/kotlin/searchSummarizer/com/Application.kt
+++ src/main/kotlin/searchSummarizer/com/Application.kt
@@ -1,26 +1,23 @@
 package searchSummarizer.com
 
-import searchSummarizer.com.presentation.plugins.configureRouting
-import searchSummarizer.com.presentation.plugins.configureSerialization
-import searchSummarizer.com.presentation.plugins.configureSockets
-import io.ktor.server.application.*
 import io.ktor.server.engine.*
+import io.ktor.server.http.content.*
 import io.ktor.server.netty.*
-import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import searchSummarizer.com.presentation.routes.registerBrowserRoutes
+import java.io.File
 
 fun main() {
 
     embeddedServer(
         Netty,
-        port = System.getenv("PORT").toInt() // 8080
+        port =  System.getenv("PORT").toInt() // 8080
     ) {
-        configureRouting()
-        configureSerialization()
-        configureSockets()
+        registerBrowserRoutes()
         routing {
-            get("/") {
-                call.respondText("Hello, world!")
+            static(".well-known") {
+                staticRootFolder = File(".well-known") // Establishes a root folder
+                file("assetlinks.json")
             }
         }
     }.start(wait = true)
--- src/main/kotlin/searchSummarizer/com/presentation/routes/BrowserRoutes.kt
+++ src/main/kotlin/searchSummarizer/com/presentation/routes/BrowserRoutes.kt
@@ -6,37 +6,9 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Route.browserRouting() {
+
     route("/summarized") {
         get("/urls") {
-            call.respondText(
-                text = """
-                    <!DOCTYPE html>
-                    <html>
-                    <head>
-                    <title>Summarized Url</title>
-                    </head>
-                    <body>
-                    <script>
-                        triggerAppOpen()
-                        function fallbackToStore() {
-                          document.write("faild");
-                        };
-                        function openApp() {
-                          // window.location.replace('http://0.0.0.0:8080/summarized/loading');
-                          // window.location.replace('https://search-summarizer.herokuapp.com/summarized/loading');
-                          window.location.replace('summarized_urls://');
-                        };
-                        function triggerAppOpen() {
-                          openApp();
-                          setTimeout(fallbackToStore, 250);
-                        };
-                    </script>
-                    </body>
-                    </html>
-                """.trimIndent(),
-                contentType = ContentType.Text.Html
-            )
-
         }
         get("/loading") {
             call.respondText("Loading", status = HttpStatusCode.Created)
